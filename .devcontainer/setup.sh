583cfc4d3a48fae9a29d3963cdc7cdd819d7d90e
clayton <ward.clayton2@gmail.com>
2025-05-22 17:11:05 -0700

    got unit tests workinggit add .devcontainer/devcontainer.json .devcontainer/setup.sh!

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,16 +4,40 @@ set -eux
 echo "Setting up vLLM environment (editable install)"
 
 cd /workspaces/vllm
+# *************** flashinfer build ***************
+# this is all a workaround for the fact that flashinfer is not available on PyPI for torch 2.7
+# remove this when it is available
+# Uninstall existing incompatible version
+pip uninstall -y flashinfer-python
 
-# # Find vLLM installed location (from pip)
-# VLLM_INSTALL_DIR=$(python3 -c 'import vllm; import os; print(os.path.dirname(vllm.__file__))')
-# echo "Found vLLM installed at: $VLLM_INSTALL_DIR"
+# Install adjusted build dependencies (setuptools fixed)
+pip install setuptools==79.0.1 packaging==23.2 ninja==1.11.1.3 build==1.2.2.post1
 
-# # Backup existing python vllm package
-# mv "$VLLM_INSTALL_DIR" "${VLLM_INSTALL_DIR}_backup"
+# Clone FlashInfer at correct commit
+git clone --recursive https://github.com/flashinfer-ai/flashinfer.git
+cd flashinfer
+git checkout 21ea1d2545f74782b91eb8c08fd503ac4c0743fc
 
-# # Symlink your source code as the installed vllm package
-# ln -s "$(pwd)/vllm" "$VLLM_INSTALL_DIR"
+# Environment for correct build
+export FLASHINFER_ENABLE_AOT=1
+export TORCH_CUDA_ARCH_LIST='8.9'
+
+# Build FlashInfer wheel
+python3 setup.py bdist_wheel --dist-dir=./dist --verbose
+
+# Install built wheel
+pip install ./dist/flashinfer_python*.whl
+# *************** flashinfer build end ***************
+
+echo "Installing vLLM dev and test dependencies..."
+pip install -r requirements/dev.txt -r requirements/test.txt
+
+# clean out the non-editable build
+pip uninstall -y vllm
+
+echo "Setting up vLLM in editable mode with precompiled kernels..."
+VLLM_USE_PRECOMPILED=1 pip install --editable . --no-build-isolation
+
 
 echo "Editable mode setup complete."
 
